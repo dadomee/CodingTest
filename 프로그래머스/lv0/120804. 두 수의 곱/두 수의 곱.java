478d67822bafb78b3f8a327b394529f94c790020
Dasom Jin <137809118+dadomee@users.noreply.github.com>
2023-08-23 18:58:54 +0900

    [level 0] Title: 두 수의 곱, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        boolean b = ( 0<=num1 && num1 <= 100&& 0<= num2 && num2<= 100);
+        int answer = 0;
+        if(b) {
+            answer = num1 * num2;
+        }
+        return answer;
+    }
+}
